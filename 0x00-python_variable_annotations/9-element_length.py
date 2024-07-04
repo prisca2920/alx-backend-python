4f79032aa48e8dd2b48a28a7467bb331ce91e33e
prisca2920 <prisca2920@gmail.com>
2024-07-04 18:12:24 +0000

    duck type

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -4,4 +4,5 @@ from typing import List, Tuple, Sequence, Iterable
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """duck type iterable obj"""
     return [(i, len(i)) for i in lst]
