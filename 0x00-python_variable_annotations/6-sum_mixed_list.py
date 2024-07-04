bb9dc947388ee2d769e54b3976af574e6d962aec
prisca2920 <prisca2920@gmail.com>
2024-07-04 11:34:55 +0000

    sum of int & float

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""sum of a mixed list"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """the sum of a mixed list"""
+    return sum(mxd_lst)
